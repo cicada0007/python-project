6558c89242c34dccf2348814dab246529af2c607
sath000007 <101625652+cicada0007@users.noreply.github.com>
2022-10-28 12:10:52 +0530

    Create threading.py
    threading in python

---
+++ threading.py
@@ -0,0 +1,16 @@
+#1/usr/bin/python3
+
+import threadinng 
+
+
+def loop1():
+    while True:
+        print("loop1")
+
+def loop2():
+    while True:
+        prinnt("loop2")
+
+t1 =threading.Threading(targrt=loop1)
+t1.start()
+loop2()        
